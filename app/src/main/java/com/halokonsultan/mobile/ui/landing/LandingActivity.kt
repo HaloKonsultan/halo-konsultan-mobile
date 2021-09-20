735c097951df216acbcef3c68f975b5600226b51
anonGTF <anongtf26@gmail.com>
2021-09-20 22:43:21 +0700

    feat(landing) handle button logic

--- app/src/main/java/com/halokonsultan/mobile/ui/landing/LandingActivity.kt
+++ app/src/main/java/com/halokonsultan/mobile/ui/landing/LandingActivity.kt
@@ -4,14 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.halokonsultan.mobile.R
+import com.halokonsultan.mobile.databinding.ActivityLandingBinding
+import com.halokonsultan.mobile.ui.login.LoginActivity
 import com.halokonsultan.mobile.ui.main.MainActivity
+import com.halokonsultan.mobile.ui.register.RegisterActivity
 
 class LandingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLandingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing)
+        binding = ActivityLandingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
     }
 }
