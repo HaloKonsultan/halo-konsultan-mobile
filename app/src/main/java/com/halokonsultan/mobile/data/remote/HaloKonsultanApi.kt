899f057176a08b308382beac91ed2ea5596570c9
anonGTF <anongtf26@gmail.com>
2021-09-28 21:14:59 +0700

    feat(register) add register logic and validation

--- app/src/main/java/com/halokonsultan/mobile/data/remote/HaloKonsultanApi.kt
+++ app/src/main/java/com/halokonsultan/mobile/data/remote/HaloKonsultanApi.kt
@@ -7,8 +7,13 @@ import retrofit2.http.POST
 
 interface HaloKonsultanApi {
 
-    @POST("register")
+    @POST("login")
     suspend fun login(
+            @Body authInfo: HashMap<String, Any>
+    ): Response<AuthResponse>
+
+    @POST("register")
+    suspend fun register(
             @Body authInfo: HashMap<String, Any>
     ): Response<AuthResponse>
 }
