c1b8398f00b7382666f39a8cf0bf1187116f6884
HelloWorld <nurhidayah282000@gmail.com>
2021-09-30 14:33:22 +0700

    Layout Detil Consultation

---
+++ app/src/main/java/com/halokonsultan/mobile/ui/consultation/DetilConsultationActivity.kt
@@ -0,0 +1,12 @@
+package com.halokonsultan.mobile.ui.consultation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.halokonsultan.mobile.R
+
+class DetilConsultationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detil_consultation)
+    }
+}
